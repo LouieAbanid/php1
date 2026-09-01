7c995de3d87142fb2466b638640fdc55d4bf0f3c
LouieAbanid <abanidlouie0@gmail.com>
2026-09-01 13:49:12 +0800

    Update profile.php

--- students/profile.php
+++ students/profile.php
@@ -1,30 +1,37 @@
+<?php
+$student = [
+    "Student ID" => "23208465",
+    "Full Name" => "Louie Abanid",
+    "Program" => "BS Information Technology",
+    "Year Level" => "3nd Year",
+    "Section" => "3C",
+    "Email" => "Louie@example.com",
+];
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Profile</title>
 
     <style>
         body {
-            margin: 0;
-            padding-top: 50px;
-            display: flex;
-            justify-content: center;
-        }
-
-        .profile {
             text-align: center;
         }
 
         table {
             border-collapse: collapse;
             width: 500px;
+            margin: 20px auto;
         }
 
         th, td {
             border: 1px solid black;
             padding: 10px;
             text-align: left;
-        }
+            }
 
         th {
             background-color: #f2f2f2;
@@ -34,30 +41,21 @@
 
 <body>
 
-    <div class="profile">
-        <h2>Student Profile</h2>
+    <h2>Student Profile</h2>
 
-        <?php
-        $student = [
-            "Student ID" => "23208465",
-            "Full Name" => "Louie O. Abanid",
-            "Program" => "BS Information Technology",
-            "Year Level" => "3rd Year",
-            "Section" => "BSIT-3C",
-            "Email" => "abanidlouie0@gmail.com",
-            "Status" => "Active"
-        ];
-        ?>
+    <table>
+        <tr>
+            <th>Information</th>
+            <th>Details</th>
+        </tr>
 
-        <table>
-            <?php foreach ($student as $key => $value): ?>
-                <tr>
-                    <th><?php echo $key; ?></th>
-                    <td><?php echo $value; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </div>
+        <?php foreach ($student as $key => $value): ?>
+            <tr>
+                <td><?php echo $key; ?></td>
+                <td><?php echo $value; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
 </body>
 </html>
